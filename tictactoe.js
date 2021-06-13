fa628c6db601ac506be57f26d7cf2e6a1f3dffb2
rickpan12 <85516330+rickpan12@users.noreply.github.com>
2021-06-12 20:05:02 -0400

    Add files via upload

---
+++ tictactoe.js
@@ -0,0 +1,25 @@
+var resetButton = document.querySelector("button");
+var squares = document.querySelectorAll('td');
+
+function resetGame(){
+    for (var i = 0; i < squares.length; i++){
+        squares[i].textContent = "";
+
+    }
+}
+
+function changeMarker(){
+    if (this.textContent === ""){
+        this.textContent = "X";
+    } else if (this.textContent === 'X'){
+        this.textContent = "O";
+    } else {
+        this.textContent = "";
+    }
+}
+
+for (var i = 0; i < squares.length; i++){
+    squares[i].addEventListener('click', changeMarker);
+}
+
+resetButton.addEventListener('click', resetGame);
